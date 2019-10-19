aff73836e2ef8e78f715ac53f793650b0e5680c3
Filip <42967255+filipzakrocki@users.noreply.github.com>
2019-10-19 16:04:21 +0100

    Authorization form added

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import classes from './NavigationItems.css';
-import NavigationItem from './NavigationItem/NavigationItem'
+import React from "react";
+import classes from "./NavigationItems.css";
+import NavigationItem from "./NavigationItem/NavigationItem";
 
-const navigationItems = (props) => (
-    <ul className={classes.NavigationItems}>
-        <NavigationItem link='/'>Burger Builder</NavigationItem>
-        <NavigationItem link='/orders' >My Orders</NavigationItem>
-        
-    </ul>
-)
+const navigationItems = props => (
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/">Burger Builder</NavigationItem>
+    <NavigationItem link="/orders">My Orders</NavigationItem>
+    <NavigationItem link="/auth">Login</NavigationItem>
+  </ul>
+);
 
 export default navigationItems;
